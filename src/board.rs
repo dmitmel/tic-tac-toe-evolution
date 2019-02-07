72d78edb8f1c794e9e08b24cb16eaf15832801ce
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-02-07 16:47:40 +0200

    add tests for board

--- src/board.rs
+++ src/board.rs
@@ -39,3 +39,58 @@ impl Board {
     self.cells[y * self.width + x] = cell;
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_constructor() {
+    let width = 123;
+    let height = 456;
+
+    let board = Board::new(width, height);
+
+    assert_eq!(board.width, width);
+    assert_eq!(board.height, height);
+    assert_eq!(board.cells.len(), width * height);
+  }
+
+  #[test]
+  fn test_cells() {
+    let x = 1;
+    let y = 1;
+
+    let mut board = Board::new(x * 2 + 1, y * 2 + 1);
+
+    assert_eq!(board.get(x, y), None);
+
+    board.set(x, y, Some(PlayerMark::X));
+    assert_eq!(board.get(x, y), Some(PlayerMark::X));
+
+    board.set(x, y, Some(PlayerMark::O));
+    assert_eq!(board.get(x, y), Some(PlayerMark::O));
+  }
+
+  #[test]
+  #[should_panic(expected = "x out of bounds")]
+  fn test_x_out_of_bounds() {
+    let width = 2;
+    let height = 2;
+
+    let board = Board::new(width, height);
+
+    board.get(width, 0);
+  }
+
+  #[test]
+  #[should_panic(expected = "y out of bounds")]
+  fn test_y_out_of_bounds() {
+    let width = 2;
+    let height = 2;
+
+    let board = Board::new(width, height);
+
+    board.get(0, height);
+  }
+}
