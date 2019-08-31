3ec67eb43891c5235e4c18fa654bc53ffa2d34d8
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-08-31 18:26:43 +0300

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 mod board;
 mod bot;
 
+use std::f64;
+
 use gio::prelude::*;
 use gtk::prelude::*;
 use gtk::Application;
 
+use crate::board::{Board, PlayerMark};
+
 const APP_ID: &str = "com.github.dmitmel.tic-tac-toe-evolution";
+
+const BOARD_CELL_SIZE: i32 = 32;
+const BOARD_MARK_MARGIN: f64 = 0.1;
+const BOARD_MARK_LINE_WIDTH: f64 = 0.25;
 
 fn main() {
   let application =
@@ -13,42 +21,17 @@ fn main() {
       .expect("failed to initialize GTK application");
 
   application.connect_activate(|app| {
-    let glade_src = include_str!("main.glade");
-    let builder = gtk::Builder::new_from_string(glade_src);
+    let builder = gtk::Builder::new_from_string(include_str!("main.glade"));
 
-    let mut board = board::Board::new(10, 10);
+    let mut board = Board::new(10, 10);
+    randomly_fill_board(&mut board);
 
-    {
-      use rand::distributions::{Bernoulli, Distribution};
-
-      let mut rng = rand::thread_rng();
-      let d = Bernoulli::new(0.5).unwrap();
-      for y in 0..board.width() {
-        for x in 0..board.height() {
-          board.set(
-            x,
-            y,
-            if d.sample(&mut rng) {
-              Some(if d.sample(&mut rng) {
-                board::PlayerMark::X
-              } else {
-                board::PlayerMark::O
-              })
-            } else {
-              None
-            },
-          )
-        }
-      }
-    }
-
-    const BOARD_CELL_SIZE: i32 = 32;
     let ui_board: gtk::DrawingArea = builder.get_object("board").unwrap();
     ui_board.set_size_request(
       board.width() as i32 * BOARD_CELL_SIZE,
       board.height() as i32 * BOARD_CELL_SIZE,
     );
-    ui_board.connect_draw(move |ui_board, ctx: &cairo::Context| {
+    ui_board.connect_draw(move |ui_board, ctx: &cairo::Context| -> Inhibit {
       let style_ctx = ui_board.get_style_context();
       gtk::render_background(
         &style_ctx,
@@ -58,46 +41,7 @@ fn main() {
         f64::from(ui_board.get_allocated_width()),
         f64::from(ui_board.get_allocated_height()),
       );
-
-      let line_width = 0.25;
-      let padding = 0.1;
-      ctx.set_line_width(line_width);
-      for y in 0..board.width() {
-        for x in 0..board.height() {
-          if let Some(mark) = board.get(x, y) {
-            use crate::board::PlayerMark;
-            let size = f64::from(BOARD_CELL_SIZE);
-            ctx.save();
-            ctx.translate(x as f64 * size, y as f64 * size);
-            ctx.scale(size, size);
-            ctx.translate(padding, padding);
-            ctx.scale(1.0 - padding, 1.0 - padding);
-            match mark {
-              PlayerMark::X => {
-                let a = line_width / std::f64::consts::SQRT_2 / 2.0;
-                ctx.set_source_rgb(239.0 / 255.0, 41.0 / 255.0, 41.0 / 255.0);
-                ctx.move_to(a, a);
-                ctx.line_to(1.0 - a, 1.0 - a);
-                ctx.move_to(a, 1.0 - a);
-                ctx.line_to(1.0 - a, a);
-              }
-              PlayerMark::O => {
-                ctx.set_source_rgb(52.0 / 255.0, 101.0 / 255.0, 164.0 / 255.0);
-                ctx.arc(
-                  0.5,
-                  0.5,
-                  0.5 - line_width / 2.0,
-                  0.0,
-                  2.0 * std::f64::consts::PI,
-                );
-              }
-            }
-            ctx.stroke();
-            ctx.restore();
-          }
-        }
-      }
-
+      render_board(&board, ctx);
       Inhibit(false)
     });
 
@@ -108,4 +52,72 @@ fn main() {
 
   let args: Vec<String> = std::env::args().collect();
   application.run(&args);
+}
+
+fn randomly_fill_board(board: &mut Board) {
+  use rand::distributions::{Bernoulli, Distribution};
+  let mut rng = rand::thread_rng();
+  let dstr = Bernoulli::new(0.5).unwrap();
+  for y in 0..board.width() {
+    for x in 0..board.height() {
+      board.set(
+        x,
+        y,
+        if dstr.sample(&mut rng) {
+          Some(if dstr.sample(&mut rng) {
+            PlayerMark::X
+          } else {
+            PlayerMark::O
+          })
+        } else {
+          None
+        },
+      )
+    }
+  }
+}
+
+fn render_board(board: &Board, ctx: &cairo::Context) {
+  ctx.set_line_width(BOARD_MARK_LINE_WIDTH);
+  for y in 0..board.width() {
+    for x in 0..board.height() {
+      if let Some(mark) = board.get(x, y) {
+        ctx.save();
+        ctx.scale(f64::from(BOARD_CELL_SIZE), f64::from(BOARD_CELL_SIZE));
+        ctx.translate(
+          x as f64 + BOARD_MARK_MARGIN,
+          y as f64 + BOARD_MARK_MARGIN,
+        );
+        ctx.scale(1.0 - BOARD_MARK_MARGIN * 2.0, 1.0 - BOARD_MARK_MARGIN * 2.0);
+
+        match mark {
+          PlayerMark::X => render_player_mark_x(ctx),
+          PlayerMark::O => render_player_mark_o(ctx),
+        }
+        ctx.stroke();
+
+        ctx.restore();
+      }
+    }
+  }
+}
+
+fn render_player_mark_x(ctx: &cairo::Context) {
+  let a = BOARD_MARK_LINE_WIDTH / f64::consts::SQRT_2 / 2.0;
+  ctx.set_source_rgb(239.0 / 255.0, 41.0 / 255.0, 41.0 / 255.0);
+  ctx.move_to(a, a);
+  ctx.line_to(1.0 - a, 1.0 - a);
+  ctx.move_to(a, 1.0 - a);
+  ctx.line_to(1.0 - a, a);
+}
+
+fn render_player_mark_o(ctx: &cairo::Context) {
+  ctx.set_source_rgb(52.0 / 255.0, 101.0 / 255.0, 164.0 / 255.0);
+  ctx.arc(
+    0.5,
+    0.5,
+    0.5 - BOARD_MARK_LINE_WIDTH / 2.0,
+    0.0,
+    2.0 * f64::consts::PI,
+  );
 }
